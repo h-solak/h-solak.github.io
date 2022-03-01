a1148cb90517c00a240a231321eb30c3e5856fd5
h-solak <hasan23solak@gmail.com>
2022-03-01 12:18:24 +0300

    Update projects
    -Added a new project I built to my portfolio

--- src/components/ProjectList.js
+++ src/components/ProjectList.js
@@ -1,60 +1,74 @@
 const ProjectList = [
-    {
-      title: 'Color Generator',
-      url: 'https://h-solak.github.io/hex-code-generator/',
-      description: "My first project... This website generates random colors by using it's own hex-code generator and sets it as the background color.",
-      tags: "HTML CSS JS",
-      objectID: 0
-    },
-    {
-      title: 'To-Do',
-      url: 'https://h-solak.github.io/to-do-list/',
-      description: "A to-do list website made with javascript. It holds the tasks in the local storage.",
-      objectID: 1
-    },
-    {
-      title: 'Take a Note',
-      url: 'https://h-solak.github.io/notes-app/',
-      description: "A take a note app that is built with react.js. It also uses small piece of JSON code to store the notes! Plus, it has a searchbar that enables you to search the notes you've taken before.",
-      objectID: 2
-    },
-    {
-      title: 'Calculator',
-      url: 'https://h-solak.github.io/calculator/',
-      description: "A cool-looking calculator, and yeah, it is working (kinda)! The purpose of this project was to learn how to transform a UI design (from figma) into a website.",
-      objectID: 3
-    },
-    {
-      title: 'Body-Mass Index Calculator',
-      url: 'https://h-solak.github.io/body-mass-index-v1/',
-      description: "A Body-Mass-Index calculator. I actually built it for myself because I check my BMI everytime.",
-      objectID: 4
-    },
-    {
-      title: 'Guess the Height Game',
-      url: 'https://h-solak.github.io/guess-the-height/',
-      description: "A simple game I made for a school project. The website generates divs with random heights and wants you to guess the height of them in pixels.",
-      objectID: 5
-    },
-    {
-      title: 'Instagram UI Clone',
-      url: 'https://h-solak.github.io/instagram-ui-clone/',
-      description: "Clone of the actual instagram website. Made with Bootstrap and it is responsive.",
-      objectID: 6
-    },
-    {
-      title: 'LinkedIn UI Clone',
-      url: 'https://h-solak.github.io/linkedin-ui-clone/',
-      description: "Responsive clone of the actual LinkedIn website.",
-      objectID: 7
-    },
-    {
-      title: 'Google UI Clone',
-      url: 'https://h-solak.github.io/google-ui-clone/',
-      description: "Clone of the actual Google website.",
-      objectID: 8
-    }
-    
-  ];
+  {
+    title: "Notes",
+    url: "https://h-solak.github.io/take-a-note/",
+    description:
+      "A notes website I built with React.js that allows you to take a note or add new tasks to check them later!",
+    tags: "React.js",
+    objectID: 0,
+  },
+  {
+    title: "Color Generator",
+    url: "https://h-solak.github.io/hex-code-generator/",
+    description:
+      "My first project... This website generates random colors by using it's own hex-code generator and sets it as the background color.",
+    tags: "HTML CSS JS",
+    objectID: 1,
+  },
+  {
+    title: "To-Do",
+    url: "https://h-solak.github.io/to-do-list/",
+    description:
+      "A to-do list website made with javascript. It holds the tasks in the local storage.",
+    objectID: 2,
+  },
+  {
+    title: "Take a Note",
+    url: "https://h-solak.github.io/notes-app/",
+    description:
+      "A take a note app that is built with react.js. It also uses small piece of JSON code to store the notes! Plus, it has a searchbar that enables you to search the notes you've taken before.",
+    objectID: 3,
+  },
+  {
+    title: "Calculator",
+    url: "https://h-solak.github.io/calculator/",
+    description:
+      "A cool-looking calculator, and yeah, it is working (kinda)! The purpose of this project was to learn how to transform a UI design (from figma) into a website.",
+    objectID: 4,
+  },
+  {
+    title: "Body-Mass Index Calculator",
+    url: "https://h-solak.github.io/body-mass-index-v1/",
+    description:
+      "A Body-Mass-Index calculator. I actually built it for myself because I check my BMI everytime.",
+    objectID: 5,
+  },
+  {
+    title: "Guess the Height Game",
+    url: "https://h-solak.github.io/guess-the-height/",
+    description:
+      "A simple game I made for a school project. The website generates divs with random heights and wants you to guess the height of them in pixels.",
+    objectID: 6,
+  },
+  {
+    title: "Instagram UI Clone",
+    url: "https://h-solak.github.io/instagram-ui-clone/",
+    description:
+      "Clone of the actual instagram website. Made with Bootstrap and it is responsive.",
+    objectID: 7,
+  },
+  {
+    title: "LinkedIn UI Clone",
+    url: "https://h-solak.github.io/linkedin-ui-clone/",
+    description: "Responsive clone of the actual LinkedIn website.",
+    objectID: 8,
+  },
+  {
+    title: "Google UI Clone",
+    url: "https://h-solak.github.io/google-ui-clone/",
+    description: "Clone of the actual Google website.",
+    objectID: 9,
+  },
+];
 
-  export default ProjectList;
+export default ProjectList;
