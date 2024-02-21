468826a0f5905715c6659daca845590e3fd7ca17
Hasan Solak <81798435+h-solak@users.noreply.github.com>
2024-02-21 03:49:00 +0300

    router removed

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 import React, { createContext, useEffect, useMemo, useState } from "react";
 import theme from "../theme";
 import { CssBaseline, ThemeProvider, createTheme } from "@mui/material";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
 /* Pages */
 import Home from "./Pages/Home/Home";
 import Layout from "./Layout/Layout";
@@ -10,18 +9,9 @@ function App() {
   return (
     <ThemeProvider theme={theme}>
       <CssBaseline />
-      <BrowserRouter>
-        <Layout>
-          <Routes>
-            <Route key="Home" path="/" element={<Home />} />
-            <Route
-              key="NotFound"
-              path="*"
-              element={<div>Bruh PAGE NOT FOUND</div>}
-            />
-          </Routes>
-        </Layout>
-      </BrowserRouter>
+      <Layout>
+        <Home />
+      </Layout>
     </ThemeProvider>
   );
 }
