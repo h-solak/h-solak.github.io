581542b2235f005f3b3916c0aa3b704ebd5757c4
Hasan Solak <81798435+h-solak@users.noreply.github.com>
2024-03-12 22:41:49 +0300

    link ux fix

--- src/Pages/Home/Components/Projects.jsx
+++ src/Pages/Home/Components/Projects.jsx
@@ -1,6 +1,13 @@
 import React, { useState } from "react";
 import { ProjectList } from "./ProjectsList";
-import { Box, Grid, Tooltip, Typography, useMediaQuery } from "@mui/material";
+import {
+  Box,
+  Grid,
+  Link,
+  Tooltip,
+  Typography,
+  useMediaQuery,
+} from "@mui/material";
 import OpenInNewIcon from "@mui/icons-material/OpenInNew";
 
 const Projects = () => {
@@ -32,32 +39,43 @@ const Projects = () => {
               borderRadius: 4,
               cursor: "pointer",
             }}
-            onClick={() => window.open(project.url, "_blank")}
+            // onClick={() => window.open(project.url, "_blank")}
             onMouseEnter={() => setShowUrlIcon(project.title)}
             onMouseLeave={() => setShowUrlIcon("")}
           >
-            <img
-              src={project.image}
-              width={"100%"}
-              height={isSmScreen ? 200 : 150}
-              style={{
-                objectFit: "fill",
-                borderRadius: 8,
-                border: "3px solid #00000020",
+            <Link
+              href={project.url}
+              target="_blank"
+              sx={{
+                color: "inherit",
+                textDecoration: "inherit",
               }}
-            />
-            <Box display={"flex"} alignItems={"center"} gap={1}>
-              <Typography
-                sx={{
-                  transition: "0.1 ease",
-                  textDecoration:
-                    showUrlIcon == project.title ? "underline" : "none",
+            >
+              <img
+                src={project.image}
+                width={"100%"}
+                height={isSmScreen ? 200 : 150}
+                style={{
+                  objectFit: "fill",
+                  borderRadius: 8,
+                  border: "3px solid #00000020",
                 }}
-              >
-                {project.title}
-              </Typography>
-              {showUrlIcon === project.title && <OpenInNewIcon fontSize="24" />}
-            </Box>
+              />
+              <Box display={"flex"} alignItems={"center"} gap={1}>
+                <Typography
+                  sx={{
+                    transition: "0.1 ease",
+                    textDecoration:
+                      showUrlIcon == project.title ? "underline" : "none",
+                  }}
+                >
+                  {project.title}
+                </Typography>
+                {showUrlIcon === project.title && (
+                  <OpenInNewIcon fontSize="24" />
+                )}
+              </Box>
+            </Link>
           </Grid>
         </Tooltip>
       ))}
